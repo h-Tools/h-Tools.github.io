74d95ff3a6c82c9b94c625390041b7f7b80a4d19
h-Tools <rylancharnock6@gmail.com>
2023-07-30 18:16:12 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,6 +1,6 @@
 const active = 0;
 for (var i = 0; i < document.links.length; i++) {
-    if ((document.links[i].href === document.URL) || (document.URL === "https://h-Tools.github.io" && document.links[i].href === "/index.html")) {
+    if ((document.links[i].href === document.URL) || (document.URL === "https://h-tools.github.io" && document.links[i].href === "/index.html")) {
         current = i;
     }
 }
