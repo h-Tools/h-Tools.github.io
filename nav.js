edf302cd6b93d728cae141ff354c2511006d7f4c
h-Tools <rylancharnock6@gmail.com>
2023-07-31 14:03:20 +0100

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,3 +1,21 @@
+var navbar = ` 
+    <div class = logo>
+        <img src="/htools.png">
+    </div>
+    <nav>
+        <ul class = "nav_links">
+            <div class = "navbox">
+                <a href ="/wallpaper.html">H Wallpaper Generator</a>
+            </div>
+            <div class = "navbox">
+                <a href ="/index.html">Home</a>
+            </div>
+            <div class = "navbox">
+                <a href ="/about.html">About</a>
+            </div>
+        </ul>
+    </nav>`;
+document.getElementById("header").innerHTML = navbar;
 const active = 0;
 for (var i = 0; i < document.links.length; i++) {
     if (document.links[i].href === document.URL) {
